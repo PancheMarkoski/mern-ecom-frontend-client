d89a6a300270174bc065a4abfac732ee3d5ea566
Panche Markoski <panchemarkoski@gmail.com>
2024-03-04 22:41:40 +0100

    change base url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,1 +1,4 @@
-export const base_url = "http://localhost:5000/api/";
+// export const base_url = "http://localhost:5000/api/";
+// // https://mern-ecom-backend-r3s6.onrender.com
+
+export const base_url = "https://mern-ecom-backend-r3s6.onrender.com/api/";
